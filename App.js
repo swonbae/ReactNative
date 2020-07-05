a8e6ef2824b7c7792759dc325e23e9c73655f8a4
swonb <47047914+swonb@users.noreply.github.com>
2020-07-05 17:12:10 -0400

    Todo App - Added style & delete function (#10)

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import Header from "./components/Header";
+import TodoItem from "./components/TodoItem";
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -8,6 +9,10 @@ export default function App() {
     { text: "create an app", key: "2" },
     { text: "play on the switch", key: "3" },
   ]);
+
+  const pressHandler = (key) => {
+    setTodos(todos.filter((todo) => todo.key != key));
+  };
 
   return (
     <View style={styles.container}>
@@ -17,7 +22,9 @@ export default function App() {
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({ item }) => <Text>{item.text}</Text>}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
           />
         </View>
       </View>
